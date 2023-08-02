96b7e0bfcb05c457d5c71faa7a89ac4be5123cbd
Ivalin Venkov <ivalinvenkov@gmail.com>
2023-08-02 09:29:55 +0300

    Upgrade to Symfony 6.2, installing api-platform 3.0

--- config/bundles.php
+++ config/bundles.php
@@ -13,5 +13,5 @@ return [
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
-    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
